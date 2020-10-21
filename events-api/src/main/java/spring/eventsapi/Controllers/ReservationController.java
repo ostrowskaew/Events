befc8a398870405d059acd069081c999566a0742
ostrowskaew <ostrowskaew@gmail.com>
2020-10-21 13:35:18 +0200

    cross orgin

--- events-api/src/main/java/spring/eventsapi/Controllers/ReservationController.java
+++ events-api/src/main/java/spring/eventsapi/Controllers/ReservationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ReservationController {
 	
